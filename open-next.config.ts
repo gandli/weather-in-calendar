8696031ab70e7ac961520c805d34e332ce02a36e
gandli <gandli@users.noreply.github.com>
2026-03-03 08:59:00 +0800

    fix: unblock cloudflare deploy by removing r2 cache override

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,5 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
-});
+// Use default incremental cache strategy to avoid mandatory R2 binding in CI deploy.
+export default defineCloudflareConfig({});
